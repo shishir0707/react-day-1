55c16f12ad1c516d000b72301cb53c3f812ccdbf
shishir0707 <shahriarshishir07@gmail.com>
2022-09-12 15:26:15 +0600

    add from

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,5 +1,7 @@
 // import React from 'react';
+import { queryAllByAttribute } from '@testing-library/react';
 import React from 'react';
+import { useState } from 'react';
 // import React, { useState, useEffect } from 'react';
 import { Button, Card } from 'react-bootstrap';
 
@@ -13,17 +15,60 @@ const About = () => {
 
 
     const [getFromData, setFromData] = React.useState({});
+    const [formErr, setFormErr] = useState({
+        name: false,
+        desc: false,
+        roll: false
+    })
 
 
     const getData = (e) => {
         // console.log(e);
         setFromData({ ...getFromData, [e.target.name]: e.target.value.trim() })
+
+        getFromData.name && setFormErr({ name: false });
     }
 
     const handleSubmit = () => {
         console.log(getFromData);
         // alert('Test')
-        setUser(getFromData)
+        setUser(getFromData);
+        if (!getFromData.name) {
+            setFormErr({ name: true })
+        } else {
+            setFormErr({ name: false })
+        }
+
+        // if (!getFromData.desc) {
+        //     setFormErr({ desc: true })
+        // } else {
+        //     setFormErr({ desc: false })
+        // }
+
+
+        //     if (!getFromData.roll) {
+        //         setFormErr({ roll: true })
+        //     } else {
+        //         setFormErr({ roll: false })
+        //     }
+    }
+
+    if (!getFromData.name || !getFromData.desc || !getFromData.roll) {
+        // console.log(!getFromData.name , !getFromData.desc , !getFromData.roll);
+        // console.log(!getFromData.name ? "name nai" : !getFromData.desc ? "desc  nai" : "roll nai");
+        // console.log(!getFromData.name ? "name nai": "");
+        // console.log(!getFromData.desc ? "desc nai": "");
+        // console.log(!getFromData.roll ? "roll nai": "");
+
+        const NAME = !getFromData.name ? "Name, " : "";
+        const DESC = !getFromData.desc ? "Description, " : "";
+        const ROLL = !getFromData.roll ? "Roll, " : "";
+
+
+        // alert(`plz fill the input ${NAME} ${DESC} ${ROLL}`);
+
+    } else {
+        setUser(getFromData);
     }
 
     return (
@@ -33,16 +78,19 @@ const About = () => {
                 <label htmlFor="name" className='d-flex flex-column'>
                     Name:
                     <input id="name" name="name" type="text" placeholder='Enter name' onChange={(e) => getData(e)} />
+                    {formErr.name && <span className='text-danger'>Name is required</span>}
                 </label>
 
                 <label htmlFor="name" className='d-flex flex-column'>
                     Description:
                     <textarea id="desc" name="desc" type="text" placeholder='Enter desc' onChange={(e) => getData(e)} />
+                    {/* {formErr.desc && <span className='text-danger'>Description is required</span>} */}
                 </label>
 
                 <label htmlFor="name" className='d-flex flex-column'>
                     Roll:
-                    <input id="roll" name="roll" type="text" placeholder='Enter roll' onChange={(e) => getData(e)} />
+                    <input id="roll" name="roll" type="number" placeholder='Enter roll' onChange={(e) => getData(e)} />
+                    {/* {formErr.roll && <span className='text-danger'>Roll is required</span>} */}
                 </label>
                 <button onClick={() => handleSubmit()}>Submit</button>
             </div>
